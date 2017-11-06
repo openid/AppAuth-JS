a5b269f13450c441fae6010cb5e7e11259ed7545
Meligy <eng.meligy@gmail.com>
2017-11-06 16:54:55 +1100

    Improve NPM package with main file and TypeScript types
    Fixes #33

---
+++ built/node_support/index.js
@@ -0,0 +1,8 @@
+"use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+Object.defineProperty(exports, "__esModule", { value: true });
+__export(require("./node_request_handler"));
+__export(require("./node_requestor"));
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi9zcmMvbm9kZV9zdXBwb3J0L2luZGV4LnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7O0FBQUEsNENBQXVDO0FBQ3ZDLHNDQUFpQyIsInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCAqIGZyb20gJy4vbm9kZV9yZXF1ZXN0X2hhbmRsZXInO1xuZXhwb3J0ICogZnJvbSAnLi9ub2RlX3JlcXVlc3Rvcic7XG4iXX0=
---
+++ src/node_support/index.ts
@@ -0,0 +1,2 @@
+export * from './node_request_handler';
+export * from './node_requestor';
