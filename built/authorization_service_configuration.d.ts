6a190b48d24781052b9e544101fb9625d387108a
Eric Musgrave <emusgrave@tricerat.com>
2019-01-17 15:12:31 -0500

    Add additional OpenID Provider Metadata fields

--- built/authorization_service_configuration.d.ts
+++ built/authorization_service_configuration.d.ts
@@ -3,11 +3,43 @@ import { Requestor } from './xhr';
  * Represents AuthorizationServiceConfiguration as a JSON object.
  */
 export interface AuthorizationServiceConfigurationJson {
+    issuer?: string;
     authorization_endpoint: string;
     token_endpoint: string;
     revocation_endpoint: string;
     end_session_endpoint?: string;
     userinfo_endpoint?: string;
+    jwks_uri?: string;
+    registration_endpoint?: string;
+    scopes_supported?: string[];
+    response_types_supported?: string[];
+    response_modes_supported?: string[];
+    grant_types_supported?: string[];
+    acr_values_supported?: string[];
+    subject_types_supported?: string[];
+    id_token_signing_alg_values_supported?: string[];
+    id_token_encryption_alg_values_supported?: string[];
+    id_token_encryption_enc_values_supported?: string[];
+    userinfo_signing_alg_values_supported?: string[];
+    userinfo_encryption_alg_values_supported?: string[];
+    userinfo_encryption_enc_values_supported?: string[];
+    request_object_signing_alg_values_supported?: string[];
+    request_object_encryption_alg_values_supported?: string[];
+    request_object_encryption_enc_values_supported?: string[];
+    token_endpoint_auth_methods_supported?: string[];
+    token_endpoint_auth_signing_alg_values_supported?: string[];
+    display_values_supported?: string[];
+    claim_types_supported?: string[];
+    claims_supported?: string[];
+    service_documentation?: string;
+    claims_locales_supported?: string[];
+    ui_locales_supported?: string[];
+    claims_parameter_supported?: boolean;
+    request_parameter_supported?: boolean;
+    request_uri_parameter_supported?: boolean;
+    require_request_uri_registration?: boolean;
+    op_policy_uri?: string;
+    op_tos_uri?: string;
 }
 /**
  * Configuration details required to interact with an authorization service.
@@ -15,18 +47,82 @@ export interface AuthorizationServiceConfigurationJson {
  * More information at https://openid.net/specs/openid-connect-discovery-1_0-17.html
  */
 export declare class AuthorizationServiceConfiguration {
+    issuer?: string;
     authorizationEndpoint: string;
     tokenEndpoint: string;
     revocationEndpoint: string;
     userInfoEndpoint?: string;
     endSessionEndpoint?: string;
+    jwksUri?: string;
+    registrationEndpoint?: string;
+    scopesSupported?: string[];
+    responseTypesSupported?: string[];
+    responseModesSupported?: string[];
+    grantTypesSupported?: string[];
+    acrValuesSupported?: string[];
+    subjectTypesSupported?: string[];
+    idTokenSigningAlgValuesSupported?: string[];
+    idTokenEncryptionAlgValuesSupported?: string[];
+    idTokenEncryptionEncValuesSupported?: string[];
+    userinfoSigningAlgValuesSupported?: string[];
+    userinfoEncryptionAlgValuesSupported?: string[];
+    userinfoEncryptionEncValuesSupported?: string[];
+    requestObjectSigningAlgValuesSupported?: string[];
+    requestObjectEncryptionAlgValuesSupported?: string[];
+    requestObjectEncryptionEncValuesSupported?: string[];
+    tokenEndpointAuthMethodsSupported?: string[];
+    tokenEndpointAuthSigningAlgValuesSupported?: string[];
+    displayValuesSupported?: string[];
+    claimTypesSupported?: string[];
+    claimsSupported?: string[];
+    serviceDocumentation?: string;
+    claimsLocalesSupported?: string[];
+    uiLocalesSupported?: string[];
+    claimsParameterSupported?: boolean;
+    requestParameterSupported?: boolean;
+    requestUriParameterSupported?: boolean;
+    requireRequestUriRegistration?: boolean;
+    opPolicyUri?: string;
+    opTosUri?: string;
     constructor(request: AuthorizationServiceConfigurationJson);
     toJson(): {
+        issuer: string | undefined;
         authorization_endpoint: string;
         token_endpoint: string;
         revocation_endpoint: string;
         end_session_endpoint: string | undefined;
         userinfo_endpoint: string | undefined;
+        jwks_uri: string | undefined;
+        registration_endpoint: string | undefined;
+        scopes_supported: string[] | undefined;
+        response_types_supported: string[] | undefined;
+        response_modes_supported: string[] | undefined;
+        grant_types_supported: string[] | undefined;
+        acr_values_supported: string[] | undefined;
+        subject_types_supported: string[] | undefined;
+        id_token_signing_alg_values_supported: string[] | undefined;
+        id_token_encryption_alg_values_supported: string[] | undefined;
+        id_token_encryption_enc_values_supported: string[] | undefined;
+        userinfo_signing_alg_values_supported: string[] | undefined;
+        userinfo_encryption_alg_values_supported: string[] | undefined;
+        userinfo_encryption_enc_values_supported: string[] | undefined;
+        request_object_signing_alg_values_supported: string[] | undefined;
+        request_object_encryption_alg_values_supported: string[] | undefined;
+        request_object_encryption_enc_values_supported: string[] | undefined;
+        token_endpoint_auth_methods_supported: string[] | undefined;
+        token_endpoint_auth_signing_alg_values_supported: string[] | undefined;
+        display_values_supported: string[] | undefined;
+        claim_types_supported: string[] | undefined;
+        claims_supported: string[] | undefined;
+        service_documentation: string | undefined;
+        claims_locales_supported: string[] | undefined;
+        ui_locales_supported: string[] | undefined;
+        claims_parameter_supported: boolean | undefined;
+        request_parameter_supported: boolean | undefined;
+        request_uri_parameter_supported: boolean | undefined;
+        require_request_uri_registration: boolean | undefined;
+        op_policy_uri: string | undefined;
+        op_tos_uri: string | undefined;
     };
     static fetchFromIssuer(openIdIssuerUrl: string, requestor?: Requestor): Promise<AuthorizationServiceConfiguration>;
 }
--- src/authorization_service_configuration.ts
+++ src/authorization_service_configuration.ts
@@ -12,6 +12,8 @@
  * limitations under the License.
  */
 
+import {boolean} from 'joi';
+
 import {JQueryRequestor, Requestor} from './xhr';
 
 
@@ -19,11 +21,43 @@ import {JQueryRequestor, Requestor} from './xhr';
  * Represents AuthorizationServiceConfiguration as a JSON object.
  */
 export interface AuthorizationServiceConfigurationJson {
+  issuer?: string;
   authorization_endpoint: string;
   token_endpoint: string;
   revocation_endpoint: string;
   end_session_endpoint?: string;
   userinfo_endpoint?: string;
+  jwks_uri?: string;
+  registration_endpoint?: string;
+  scopes_supported?: string[];
+  response_types_supported?: string[];
+  response_modes_supported?: string[];
+  grant_types_supported?: string[];
+  acr_values_supported?: string[];
+  subject_types_supported?: string[];
+  id_token_signing_alg_values_supported?: string[];
+  id_token_encryption_alg_values_supported?: string[];
+  id_token_encryption_enc_values_supported?: string[];
+  userinfo_signing_alg_values_supported?: string[];
+  userinfo_encryption_alg_values_supported?: string[];
+  userinfo_encryption_enc_values_supported?: string[];
+  request_object_signing_alg_values_supported?: string[];
+  request_object_encryption_alg_values_supported?: string[];
+  request_object_encryption_enc_values_supported?: string[];
+  token_endpoint_auth_methods_supported?: string[];
+  token_endpoint_auth_signing_alg_values_supported?: string[];
+  display_values_supported?: string[];
+  claim_types_supported?: string[];
+  claims_supported?: string[];
+  service_documentation?: string;
+  claims_locales_supported?: string[];
+  ui_locales_supported?: string[];
+  claims_parameter_supported?: boolean;
+  request_parameter_supported?: boolean;
+  request_uri_parameter_supported?: boolean;
+  require_request_uri_registration?: boolean;
+  op_policy_uri?: string;
+  op_tos_uri?: string;
 }
 
 /**
@@ -44,27 +78,130 @@ const OPENID_CONFIGURATION = 'openid-configuration';
  * More information at https://openid.net/specs/openid-connect-discovery-1_0-17.html
  */
 export class AuthorizationServiceConfiguration {
+  issuer?: string;
   authorizationEndpoint: string;
   tokenEndpoint: string;
   revocationEndpoint: string;
   userInfoEndpoint?: string;
   endSessionEndpoint?: string;
+  jwksUri?: string;
+  registrationEndpoint?: string;
+  scopesSupported?: string[];
+  responseTypesSupported?: string[];
+  responseModesSupported?: string[];
+  grantTypesSupported?: string[];
+  acrValuesSupported?: string[];
+  subjectTypesSupported?: string[];
+  idTokenSigningAlgValuesSupported?: string[];
+  idTokenEncryptionAlgValuesSupported?: string[];
+  idTokenEncryptionEncValuesSupported?: string[];
+  userinfoSigningAlgValuesSupported?: string[];
+  userinfoEncryptionAlgValuesSupported?: string[];
+  userinfoEncryptionEncValuesSupported?: string[];
+  requestObjectSigningAlgValuesSupported?: string[];
+  requestObjectEncryptionAlgValuesSupported?: string[];
+  requestObjectEncryptionEncValuesSupported?: string[];
+  tokenEndpointAuthMethodsSupported?: string[];
+  tokenEndpointAuthSigningAlgValuesSupported?: string[];
+  displayValuesSupported?: string[];
+  claimTypesSupported?: string[];
+  claimsSupported?: string[];
+  serviceDocumentation?: string;
+  claimsLocalesSupported?: string[];
+  uiLocalesSupported?: string[];
+  claimsParameterSupported?: boolean;
+  requestParameterSupported?: boolean;
+  requestUriParameterSupported?: boolean;
+  requireRequestUriRegistration?: boolean;
+  opPolicyUri?: string;
+  opTosUri?: string;
 
   constructor(request: AuthorizationServiceConfigurationJson) {
+    this.issuer = request.issuer;
     this.authorizationEndpoint = request.authorization_endpoint;
     this.tokenEndpoint = request.token_endpoint;
     this.revocationEndpoint = request.revocation_endpoint;
     this.userInfoEndpoint = request.userinfo_endpoint;
     this.endSessionEndpoint = request.end_session_endpoint;
+    this.jwksUri = request.jwks_uri;
+    this.registrationEndpoint = request.registration_endpoint;
+    this.scopesSupported = request.scopes_supported;
+    this.responseTypesSupported = request.response_types_supported;
+    this.responseModesSupported = request.response_modes_supported;
+    this.grantTypesSupported = request.grant_types_supported;
+    this.acrValuesSupported = request.acr_values_supported;
+    this.subjectTypesSupported = request.subject_types_supported;
+    this.idTokenSigningAlgValuesSupported = request.id_token_signing_alg_values_supported;
+    this.idTokenEncryptionAlgValuesSupported = request.id_token_encryption_alg_values_supported;
+    this.idTokenEncryptionEncValuesSupported = request.id_token_encryption_enc_values_supported;
+    this.userinfoSigningAlgValuesSupported = request.userinfo_signing_alg_values_supported;
+    this.userinfoEncryptionAlgValuesSupported = request.userinfo_encryption_alg_values_supported;
+    this.userinfoEncryptionEncValuesSupported = request.userinfo_encryption_enc_values_supported;
+    this.requestObjectSigningAlgValuesSupported =
+        request.request_object_signing_alg_values_supported;
+    this.requestObjectEncryptionAlgValuesSupported =
+        request.request_object_encryption_alg_values_supported;
+    this.requestObjectEncryptionEncValuesSupported =
+        request.request_object_encryption_enc_values_supported;
+    this.tokenEndpointAuthMethodsSupported = request.token_endpoint_auth_methods_supported;
+    this.tokenEndpointAuthSigningAlgValuesSupported =
+        request.token_endpoint_auth_signing_alg_values_supported;
+    this.displayValuesSupported = request.display_values_supported;
+    this.claimTypesSupported = request.claim_types_supported;
+    this.claimsSupported = request.claims_supported;
+    this.serviceDocumentation = request.service_documentation;
+    this.claimsLocalesSupported = request.claims_locales_supported;
+    this.uiLocalesSupported = request.ui_locales_supported;
+    this.claimsParameterSupported = request.claims_parameter_supported;
+    this.requestParameterSupported = request.request_parameter_supported;
+    this.requestUriParameterSupported = request.request_uri_parameter_supported;
+    this.requireRequestUriRegistration = request.require_request_uri_registration;
+    this.opPolicyUri = request.op_policy_uri;
+    this.opTosUri = request.op_tos_uri;
   }
 
   toJson() {
     return {
+      issuer: this.issuer,
       authorization_endpoint: this.authorizationEndpoint,
       token_endpoint: this.tokenEndpoint,
       revocation_endpoint: this.revocationEndpoint,
       end_session_endpoint: this.endSessionEndpoint,
-      userinfo_endpoint: this.userInfoEndpoint
+      userinfo_endpoint: this.userInfoEndpoint,
+      jwks_uri: this.jwksUri,
+      registration_endpoint: this.registrationEndpoint,
+      scopes_supported: this.scopesSupported,
+      response_types_supported: this.responseTypesSupported,
+      response_modes_supported: this.responseModesSupported,
+      grant_types_supported: this.grantTypesSupported,
+      acr_values_supported: this.acrValuesSupported,
+      subject_types_supported: this.subjectTypesSupported,
+      id_token_signing_alg_values_supported: this.idTokenSigningAlgValuesSupported,
+      id_token_encryption_alg_values_supported: this.idTokenEncryptionAlgValuesSupported,
+      id_token_encryption_enc_values_supported: this.idTokenEncryptionEncValuesSupported,
+      userinfo_signing_alg_values_supported: this.userinfoSigningAlgValuesSupported,
+      userinfo_encryption_alg_values_supported: this.userinfoEncryptionAlgValuesSupported,
+      userinfo_encryption_enc_values_supported: this.userinfoEncryptionEncValuesSupported,
+      request_object_signing_alg_values_supported: this.requestObjectSigningAlgValuesSupported,
+      request_object_encryption_alg_values_supported:
+          this.requestObjectEncryptionAlgValuesSupported,
+      request_object_encryption_enc_values_supported:
+          this.requestObjectEncryptionEncValuesSupported,
+      token_endpoint_auth_methods_supported: this.tokenEndpointAuthMethodsSupported,
+      token_endpoint_auth_signing_alg_values_supported:
+          this.tokenEndpointAuthSigningAlgValuesSupported,
+      display_values_supported: this.displayValuesSupported,
+      claim_types_supported: this.claimTypesSupported,
+      claims_supported: this.claimsSupported,
+      service_documentation: this.serviceDocumentation,
+      claims_locales_supported: this.claimsLocalesSupported,
+      ui_locales_supported: this.uiLocalesSupported,
+      claims_parameter_supported: this.claimsParameterSupported,
+      request_parameter_supported: this.requestParameterSupported,
+      request_uri_parameter_supported: this.requestUriParameterSupported,
+      require_request_uri_registration: this.requireRequestUriRegistration,
+      op_policy_uri: this.opPolicyUri,
+      op_tos_uri: this.opTosUri
     };
   }
 
